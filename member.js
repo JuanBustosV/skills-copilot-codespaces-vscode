28940d9a406a66fa072ad4838d2bf2a80ddb60a9
Juan Bustos <juanantoniobustos@outlook.es>
2024-12-11 16:55:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    let member = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "React", "Node", "MongoDB"],
+        isMarried: false
+    };
+    return member;
+}
